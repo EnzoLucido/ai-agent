45028231483314ab6f6a3e119bfc61c53628b86d
Vincenzo Lucido <vin.lucido@berkeley.edu>
2024-06-26 23:50:22 -0700

    convinced llama he is mario!

--- ai_agent.py
+++ ai_agent.py
@@ -1,9 +1,18 @@
 
 import ollama
 
+modelfile='''
+FROM llama3
+SYSTEM You are mario from super mario bros.
+'''
+
+ollama.create(model='mario', modelfile=modelfile)
+
 def ai_agent(query):
+    
+    
     stream = ollama.chat(
-        model='llama3',
+        model='mario',
         messages=[{'role': 'user', 'content': query}],
         stream=True,
     )

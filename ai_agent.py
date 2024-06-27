82a0ea57e0bceb19546c193af64342221f14b489
Vincenzo Lucido <vin.lucido@berkeley.edu>
2024-06-27 10:39:06 -0700

    got AI to reply searching the web

--- ai_agent.py
+++ ai_agent.py
@@ -3,7 +3,8 @@ import ollama
 
 modelfile='''
 FROM llama3
-SYSTEM You are mario from super mario bros.
+SYSTEM If the user asks you for information about something that has happened on or after January 1st, 2023 reply "searching the web" and then summarize the user's query into a short search phrase. You may assume that today is in May 2024. 
+".
 '''
 
 ollama.create(model='mario', modelfile=modelfile)
@@ -17,5 +18,6 @@ def ai_agent(query):
         stream=True,
     )
 
+    print("AI: ")
     for chunk in stream:
         print(chunk['message']['content'], end='', flush=True)

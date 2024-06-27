4146c9bfcf74d8c8fa42939885daab89201e197d
Vincenzo Lucido <vin.lucido@berkeley.edu>
2024-06-27 15:16:11 -0700

    adding classifier

--- ai_agent.py
+++ ai_agent.py
@@ -2,25 +2,21 @@
 import ollama
 
 def ai_agent(query):
-    
-    response = []
+
+    classification = ollama.chat(
+        model='classifier',
+        messages=[{'role':'user', 'content': query}]
+    )
     
     stream = ollama.chat(
-        model='surfing_llama',
-        messages=[{'role': 'user', 'content': query}],
+        model='llama3',
+        messages=[{'role': 'user', 'content': cleared_query}],
         stream=True,
     )
-
-    print("AI: ")
     for chunk in stream:
         print(chunk['message']['content'], end='', flush=True)
-        response.append(chunk['message']['content'])
-    
+        
     # Join the response chunks into a single string
-    full_response = ''.join(response)
+
     
     # Check if the first words are "surfing the web"
-    if full_response.lower().startswith("surfing the web"):
-        print("The response starts with 'surfing the web'.")
-    else:
-        print("The response does not start with 'surfing the web'.")
